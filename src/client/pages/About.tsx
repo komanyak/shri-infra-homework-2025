fe1c6a15d72c82ec6b6307786fabf1733bc43604
Komanyak Alexandr <komanyak.alex@yandex.ru>
2025-07-13 05:21:14 +0300

    error fixed

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -12,7 +12,7 @@ export const About: React.FC = () => {
         <div className="col">
           <h1>About</h1>
           <p>
-            Authorrr: [Komanyak Alexander]
+            Author: [Komanyak Alexander]
           </p>
         </div>
       </div>
